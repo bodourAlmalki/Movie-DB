6cbcfaef9f7ff6106184bb77d74ab29d3116451d
Bodour Almalki <bodouralmalkisy@gmail.com>
2023-01-07 22:28:34 +0200

    Step 5

--- index.js
+++ index.js
@@ -33,5 +33,32 @@ app.get('/search', (req, res) => {
     }
   })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!')) 
 
+const movies = [ 
+{ title: 'Jaws', year: 1975, rating: 8 }, 
+{ title: 'Avatar', year: 2009, rating: 7.8 }, 
+{ title: 'Brazil', year: 1985, rating: 8 }, 
+{ title: 'الإرهاب والكباب', year: 1992, rating: 6.2 } 
+]
+
+app.get('/movies/create', (req, res) => {
+    // Handle movie create
+    res.send('Movie create')
+  })
+  
+  app.get('/movies/read', (req, res) => {
+    res.json({ status: 200, data: movies })
+  })
+  
+  app.get('/movies/update', (req, res) => {
+    // Handle movie update
+    res.send('Movie update')
+  })
+  
+  app.get('/movies/delete', (req, res) => {
+    // Handle movie delete
+    res.send('Movie delete')
+  })
+  
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  
