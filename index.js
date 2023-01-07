c56e2e40d9ec7701d1d4e3705e6023eacb96bc04
Bodour Almalki <bodouralmalkisy@gmail.com>
2023-01-07 22:18:49 +0200

    Step 4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => res.send('OK'))
 //http://localhost:3000/test
 app.get('/test', (req, res) => res.json({ status: 200, message: 'OK' }))
 
-//http://localhost:3000/test/time/
+//http://localhost:3000/time/
 
 app.get('/time', (req, res) => {
   const currentTime = new Date()
@@ -17,7 +17,21 @@ app.get('/time', (req, res) => {
 
   res.json({ status: 200, message: `${hours}:${minutes}` })
 })
+//http://localhost:3000/hello/:id(anything)/
+app.get('/hello/:id', (req, res) => {
+  const { id } = req.params
+  res.json({ status: 200, message: `Hello, ${id || 'world'}` })
+})
 
+//http://localhost:3000/search/
+app.get('/search', (req, res) => {
+    const { s } = req.query
+    if (s) {
+      res.json({ status: 200, message: 'OK', data: s })
+    } else {
+      res.status(500).json({ error: true, message: 'You have to provide a search' })
+    }
+  })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!')) 
 
