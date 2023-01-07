7c75a6d8365199a433e22abd5365ebdd0c8b91b6
Bodour Almalki <bodouralmalkisy@gmail.com>
2023-01-07 21:43:28 +0200

    Step 3

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 
+
+// http://localhost:3000/
 app.get('/', (req, res) => res.send('OK'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//http://localhost:3000/test
+app.get('/test', (req, res) => res.json({ status: 200, message: 'OK' }))
+
+//http://localhost:3000/test/time/
+
+app.get('/time', (req, res) => {
+  const currentTime = new Date()
+  const hours = currentTime.getHours()
+  const minutes = currentTime.getMinutes()
+
+  res.json({ status: 200, message: `${hours}:${minutes}` })
+})
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!')) 
+
